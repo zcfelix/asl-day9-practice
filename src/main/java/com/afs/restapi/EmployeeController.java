4c862dfb84bbb3c8943038465fa99d1032a1f77b
qicaisheng <qicaisheng@qq.com>
2023-07-18 15:44:02 +0800

    feat: impl getEmployeeById api

--- src/main/java/com/afs/restapi/EmployeeController.java
+++ src/main/java/com/afs/restapi/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.afs.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,13 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees() {
         return EMPLOYEES;
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return EMPLOYEES.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
