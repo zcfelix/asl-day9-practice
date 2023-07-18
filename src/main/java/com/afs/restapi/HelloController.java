8a1095b3bf4a687fd970d828cb1583389c56e410
qicaisheng <qicaisheng@qq.com>
2023-07-18 15:35:04 +0800

    feat: add hello controller

---
+++ src/main/java/com/afs/restapi/HelloController.java
@@ -0,0 +1,17 @@
+package com.afs.restapi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping(value = "/{name}", params = {"language"})
+    public String hello(@PathVariable String name,
+                        @RequestParam String language) {
+        if ("ZH".equals(language)) {
+            return "你好： " + name;
+        }
+        return "hello: " + name;
+    }
+}
