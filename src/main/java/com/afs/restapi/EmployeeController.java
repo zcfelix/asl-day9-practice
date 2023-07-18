509e19d81c363bd69ad9725d69145e7326a88330
qicaisheng <qicaisheng@qq.com>
2023-07-18 15:46:56 +0800

    feat: add getEmployeesByGender api

--- src/main/java/com/afs/restapi/EmployeeController.java
+++ src/main/java/com/afs/restapi/EmployeeController.java
@@ -1,11 +1,10 @@
 package com.afs.restapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -24,5 +23,12 @@ public class EmployeeController {
                 .filter(employee -> employee.getId().equals(id))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @GetMapping(params = "gender")
+    public List<Employee> getEmployeesByGender(@RequestParam String gender) {
+        return EMPLOYEES.stream()
+                .filter(employee -> employee.getGender().equals(gender))
+                .collect(Collectors.toList());
     }
 }
