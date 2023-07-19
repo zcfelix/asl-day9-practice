5b73939e71a9b211c3d8dff9ed2d48644293ee19
qicaisheng <qicaisheng@qq.com>
2023-07-19 16:53:44 +0800

    refactor: extract EmployeeRepository

--- src/main/java/com/afs/restapi/EmployeeController.java
+++ src/main/java/com/afs/restapi/EmployeeController.java
@@ -2,57 +2,40 @@ package com.afs.restapi;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    public final List<Employee> employees = new ArrayList<>();
     private final EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    public EmployeeController() {
+        this.employeeRepository = new EmployeeRepository();
     }
 
     public List<Employee> getEmployees() {
-        return employees;
+        return employeeRepository.getEmployees();
     }
 
     @GetMapping
     public List<Employee> getAllEmployees() {
-        return getEmployees();
+        return employeeRepository.getEmployees();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
-        return getEmployees().stream()
-                .filter(employee -> employee.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        return employeeRepository.findById(id);
     }
 
     @GetMapping(params = "gender")
     public List<Employee> getEmployeesByGender(@RequestParam String gender) {
-        return getEmployees().stream()
-                .filter(employee -> employee.getGender().equals(gender))
-                .collect(Collectors.toList());
+        return employeeRepository.findAllByGender(gender);
     }
 
     @PostMapping
     public void createEmployee(@RequestBody Employee employee) {
-        employee.setId(nextId());
-        getEmployees().add(employee);
-    }
-
-    private Long nextId() {
-        long maxId = getEmployees().stream()
-                .mapToLong(Employee::getId)
-                .max()
-                .orElse(0L);
-        return maxId + 1;
+        employeeRepository.insert(employee);
     }
 
 }
--- src/main/java/com/afs/restapi/EmployeeRepository.java
+++ src/main/java/com/afs/restapi/EmployeeRepository.java
@@ -2,10 +2,40 @@ package com.afs.restapi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeRepository {
 
     private List<Employee> employees = new ArrayList<>();
 
 
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public Employee findById(Long id) {
+        return getEmployees().stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Employee> findAllByGender(String gender) {
+        return getEmployees().stream()
+                .filter(employee -> employee.getGender().equals(gender))
+                .collect(Collectors.toList());
+    }
+
+    public void insert(Employee employee) {
+        employee.setId(nextId());
+        getEmployees().add(employee);
+    }
+
+    private Long nextId() {
+        long maxId = getEmployees().stream()
+                .mapToLong(Employee::getId)
+                .max()
+                .orElse(0L);
+        return maxId + 1;
+    }
 }
